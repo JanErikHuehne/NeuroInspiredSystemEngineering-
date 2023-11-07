7697f269ae8fb4e799b9f80bba5f39100dd32185
Jan-Eirk Hühne <jan@huehne-lenthe.de>
2023-11-07 16:35:02 +0100

    integrated mediapipe

--- client.py
+++ client.py
@@ -5,6 +5,10 @@ from threading import Thread, Event
 import tkinter.ttk as ttk
 import time
 import socket
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np 
+import math 
+import copy 
 
 
 class Client:
@@ -39,12 +43,19 @@ class Client:
         self.stop_button['state'] = tk.DISABLED
         # Video camera feed
         self.cap = cv2.VideoCapture(0)
+        self.width, self.height = 1280, 720
+
+        # Camera
+       
+        self.cap.set(3, self.width) # setting width
+        self.cap.set(4, self.height) # setting height
         # Char and Word text
         self.char = tk.Label(self.root, text="Current character", bd=0, bg="white", font=("'Segoe UI", 24) )
         self.char_moving = tk.Label(self.root, text="", bd=0, bg="white", font=("'Segoe UI", 24, "bold") )
         self.collect_next_char_event = Event()
         self.collect_next_char_event.clear()
         self.next_char_collect_event = Event()
+        self.save_file = "./two.txt"
         self.next_char_collect_event.clear()
         self.word_var = tk.StringVar()
         self.word_text = tk.Label(self.root, text="Word ", bd=0, bg="white", font=("'Segoe UI", 24))
@@ -53,11 +64,17 @@ class Client:
         # next char bar
         self.var  = tk.IntVar()
         self.progessbar = ttk.Progressbar(self.root, variable=self.var, orient=tk.HORIZONTAL, length=250)  
-       
+
         # self udp_client
         self.udp_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+        #detector
+        self.detector = HandDetector(maxHands = 2, detectionCon = 0.8) 
+
+        # start app 
         self.root.mainloop()
-  
+
+        
         
 
     def add_values(self):
@@ -104,25 +121,138 @@ class Client:
         self.collect_char_thread = Thread(target=self.collect_char)
         self.collect_char_thread.start()
 
+
+    def media_pipe_detection(self,):
+        hands, _ = self.detector.findHands(copy.deepcopy(self.frame))
+        data = [] # a list of data points we are sending to unity
+
+        # we are sending a landmark list -> 21 in total, every has 3 coordinates: x,y,z
+        if hands:
+            for hand in hands:
+                # Get the list of landmarks (points)
+                lmList = hand['lmList']
+                # print(lmList)
+    
+                # print(hand) # this is a dictionary; we are interested in lmList and type
+                # but it also prints out bbox and center of it
+
+                # get the detected hand -> left or right ofc
+                # handtype = hand['type']
+                # print(handtype)
+
+                # since we are sending coordinates to unity, we need to
+                # split them so that they are contained in only one list
+                # without being split in sublists  
+                for lm in lmList:
+                    data.extend([lm[0], self.height - lm[1], lm[2]]) # upper left corner = (0,0) in opencv; lower right corner = (0,0) in unity
+                # processing data and sending clean data to unity project
+    #             print(lmList)
+                Handpoints = np.array(lmList)
+    #             print(Handpoints)
+                EU = np.array(self.calculateDistances(Handpoints))
+                EU /= EU.max()
+                print(EU)
+                #print(EU[9])
+            
+                return self.detectHandMovement(EU)
+        return "Empty"
+    def calculateDistances(self, handPoints):
+        eu = [0.0] * 11
+        # thumb to index
+        eu[0] = math.sqrt((handPoints[4,0] - handPoints[8,0])**2 + (handPoints[4,1] - handPoints[8,1])**2 + (handPoints[4,2] - handPoints[8,2])**2)
+        # thumb to middle
+        eu[1] = math.sqrt((handPoints[4,0] - handPoints[12,0])**2 + (handPoints[4,1] - handPoints[12,1])**2 + (handPoints[4,2] - handPoints[12,2])**2)
+        # thumb to ring
+        eu[2] = math.sqrt((handPoints[4,0] - handPoints[16,0])**2 + (handPoints[4,1] - handPoints[16,1])**2 + (handPoints[4,2] - handPoints[16,2])**2)
+        # thumb to pinky
+        eu[3] = math.sqrt((handPoints[4,0] - handPoints[20,0])**2 + (handPoints[4,1] - handPoints[20,1])**2 + (handPoints[4,2] - handPoints[20,2])**2)
+        # ring to pinky
+        eu[4] = math.sqrt((handPoints[16,0] - handPoints[20,0])**2 + (handPoints[16,1] - handPoints[20,1])**2 + (handPoints[16,2] - handPoints[20,2])**2)
+        # palm to thumb
+        eu[5] = math.sqrt((handPoints[0,0] - handPoints[4,0])**2 + (handPoints[0,1] - handPoints[4,1])**2 + (handPoints[0,2] - handPoints[4,2])**2)
+        eu[6] = math.sqrt((handPoints[0,0] - handPoints[8,0])**2 + (handPoints[0,1] - handPoints[8,1])**2 + (handPoints[0,2] - handPoints[8,2])**2)
+        eu[7] = math.sqrt((handPoints[0,0] - handPoints[12,0])**2 + (handPoints[0,1] - handPoints[12,1])**2 + (handPoints[0,2] - handPoints[12,2])**2)
+        eu[8] = math.sqrt((handPoints[0,0] - handPoints[16,0])**2 + (handPoints[0,1] - handPoints[16,1])**2 + (handPoints[0,2] - handPoints[16,2])**2)
+        eu[9] = math.sqrt((handPoints[0,0] - handPoints[20,0])**2 + (handPoints[0,1] - handPoints[20,1])**2 + (handPoints[0,2] - handPoints[20,2])**2)
+        eu[10] = math.sqrt((handPoints[4,0] - handPoints[17,0])**2 + (handPoints[4,1] - handPoints[17,1])**2 + (handPoints[4,2] - handPoints[17,2])**2)
+        return eu
+    
+
+    def detectHandMovement(self, eu):
+        # Calculate distances between hand points
+        # Check the distances, compare with threshold, detect the hand movement
+        # and play corresponding chords based on thresholds
+        # ZERO
+        if (eu[0] < 40) and (eu[1] < 40) and (eu[2] < 40) and (eu[3] < 40):
+            sign = '0'
+            print(sign)
+        # ONE
+        #elif (eu[0] > 80) and (eu[1] < 50) and (eu[2] < 70) and (eu[3] < 80):
+        elif (eu[5] > 0.8) and (eu[6] < 0.8) and (eu[4] < 0.2):
+            sign = '1'
+            print(sign)
+        # TWO
+        #elif (eu[0] > 70) and (eu[1] > 7) and (eu[2] < 40) and (eu[3] < 40) and (eu[4] < 30):
+        elif (eu[5] > 0.8) and (eu[6] > 0.8) and (eu[4] < 0.2):
+            sign = '2'
+            print(sign)
+        # THREE 
+        elif (eu[5] > 60) and (eu[6] > 80) and (eu[7] > 80) and (eu[8] < 90) and (eu[9] < 90) and ( eu[4] < 40):
+                sign = '3'
+                print(sign)
+
+        elif (eu[5] > 60) and (eu[6] > 50) and (eu[7] > 100) and (eu[8] > 100) and (eu[9] > 140) and (eu[10]< 30):
+                sign = '4'
+                print(sign)
+
+        #FIVE (all fingertips must be away from origin)
+        elif (eu[0] > 80) and (eu[1] > 80) and (eu[2] > 80) and (eu[3] > 80) and (eu[5:9]> [100,100,100,100,100]):
+            sign = '5'
+            print(sign)
+
+        # SIX
+        elif (eu[0] > 30) and (eu[1] > 30) and (eu[2] > 30) and (eu[3] < 30):
+            sign = '6'
+            print(sign)
+
+        # SEVEN
+        elif (eu[0] > 30) and (eu[1] > 30) and (eu[2] < 30) and (eu[3] > 30) and (eu[4] > 30):
+            sign = '7'
+            print(sign)
+
+        # EIGHT
+        elif (eu[0] > 30) and (eu[1] < 30) and (eu[2] > 30) and (eu[3] > 30):
+            sign = '8'
+            print(sign)
+
+        # NINE
+        elif (eu[0] < 30) and (eu[1] > 30) and (eu[2] > 30) and (eu[3] > 30):
+            sign = '9'
+            print(sign)
+        else:
+            sign = 'None'   
+        return sign
+
     def collect_char(self,):
         while not self.stop_button_event.is_set():
             self.next_char_collect_event.clear()
             self.collect_next_char_event.wait()
             # Do something with same frame, pass it to mediapipe in this thread
             print("collected")
-            if self.counter < 5: 
-                collected_char  = "T"
-                self.counter += 1
-            else:
-                collected_char = " "
-                self.counter = 0
-            
-            if collected_char == " ":
+           
+            collected_char = self.media_pipe_detection()
+            print(collected_char)
+            if collected_char == "None":
                 # UDP thread to send 
-                self.udp_send_thread = Thread(target=self.udp_send, args=(self.word_var.get(),))
-                self.udp_send_thread.start()
+                #self.udp_send_thread = Thread(target=self.udp_send, args=(self.word_var.get(),))
+                #self.udp_send_thread.start()
                 self.word_var.set("")
-            self.word_var.set(self.word_var.get() + collected_char)
+            elif collected_char == "Empty":
+                print("No character collected")
+            else:
+                self.word_var.set(self.word_var.get() + collected_char)
+            
+            
             self.next_char_collect_event.set()
             time.sleep(0.1)
 
@@ -149,10 +279,10 @@ class Client:
     def show_frame(self,):
         # get frame
         ret, self.frame = self.cap.read()
-   
+        hands, frame = self.detector.findHands(self.frame)
         if ret:
             # cv2 uses `BGR` but `GUI` needs `RGB`
-            frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
             # convert to PIL image
             img = Image.fromarray(frame).resize((550, 400))
