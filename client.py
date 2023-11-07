871b915e62a59ee2a0bf5ec537250d321fb634f1
Jan-Eirk Hühne <jan@huehne-lenthe.de>
2023-11-07 12:17:55 +0100

    updated client

--- client.py
+++ client.py
@@ -4,28 +4,7 @@ import cv2
 from threading import Thread, Event
 import tkinter.ttk as ttk
 import time
-"""
 import socket
-serverAddress = input("Enter server address:")
-serverAddressPort = (serverAddress, 20001)
-
-
-msgFromClient       = "Hello UDP Server"
-bytesToSend         = str.encode(msgFromClient)
-
-bufferSize = 1024
-while True:
-
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    message = input("Enter message to send:")
-    bytesToSend         = str.encode(message)
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    msg = "Message from Server {}".format(msgFromServer[0])
-    print(msg)
-
-"""
-
 
 
 class Client:
@@ -41,12 +20,14 @@ class Client:
         self.camera.grid(row=0, column=2, padx=(50, 0), pady=(50,0), rowspan=100, columnspan=200)
         self.address_label = tk.Label(self.root, text="Server IP Adress ", bd=0, bg="white").grid(row=0, column=0, pady=(50,0))
         self.port_label = tk.Label(self.root, text="Server Port ", bd=0, bg="white").grid(row=1, column=0) 
-        self.server_ip = ""
-        self.server_port = ""
+        self.server_ip = tk.StringVar()
+        self.server_port = tk.StringVar()
         self.current_word_text = ""
         # Ip and port entry fields 
-        self.server_ip_entry = tk.Entry(self.root, textvariable=self.server_ip, ).grid(row=0, column=1, pady=(50,0))
-        self.server_port_entry = tk.Entry(self.root, textvariable=self.server_port).grid(row=1, column=1)
+        self.server_ip_entry = tk.Entry(self.root, textvariable=self.server_ip, )
+        self.server_ip_entry.grid(row=0, column=1, pady=(50,0))
+        self.server_port_entry = tk.Entry(self.root, textvariable=self.server_port)
+        self.server_port_entry.grid(row=1, column=1)
         # Start button 
         self.start_button = tk.Button(self.root, text ="Start",borderwidth=0, command = self.start_button_pressed, height= 3, width=30)
         self.start_button.grid(row=3, column=0, columnspan = 2, pady=(230, 0))
@@ -68,11 +49,13 @@ class Client:
         self.word_var = tk.StringVar()
         self.word_text = tk.Label(self.root, text="Word ", bd=0, bg="white", font=("'Segoe UI", 24))
         self.word_text_moving = tk.Label(self.root, textvariable=self.word_var, bd=0, bg="white", font=("'Segoe UI", 24, "bold") ) 
-    
+        self.counter = 0
         # next char bar
         self.var  = tk.IntVar()
         self.progessbar = ttk.Progressbar(self.root, variable=self.var, orient=tk.HORIZONTAL, length=250)  
        
+        # self udp_client
+        self.udp_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.root.mainloop()
   
         
@@ -94,7 +77,13 @@ class Client:
             self.add_values()
 
     def start_button_pressed(self):
+        print(self.server_ip.get())
+        print(self.server_port.get())
+        self.server_ip_entry['state'] = tk.DISABLED
+        self.server_port_entry['state'] = tk.DISABLED
         self.stop_button_event.clear()
+        self.word_var.set("")
+        self.counter = 0 
         self.camera = tk.Label(self.root)
         self.camera.grid(row=0, column=2, padx=(50, 0), pady=(50,0), rowspan=100, columnspan=200)
         self.char.grid(row=101, column=2, padx=(50, 0), pady=(30,0))
@@ -104,6 +93,8 @@ class Client:
         self.progessbar.grid(row=101, rowspan=4, column=0, columnspan=2, pady=(20, 0))
         self.start_button['state'] = tk.DISABLED
         self.stop_button['state'] = tk.NORMAL
+
+
         self.camera_thread = Thread(target=self.show_frame())
         self.camera_thread.start()
 
@@ -119,14 +110,33 @@ class Client:
             self.collect_next_char_event.wait()
             # Do something with same frame, pass it to mediapipe in this thread
             print("collected")
-            self.word_var.set(self.word_var.get()+  "T")
+            if self.counter < 5: 
+                collected_char  = "T"
+                self.counter += 1
+            else:
+                collected_char = " "
+                self.counter = 0
+            
+            if collected_char == " ":
+                # UDP thread to send 
+                self.udp_send_thread = Thread(target=self.udp_send, args=(self.word_var.get(),))
+                self.udp_send_thread.start()
+                self.word_var.set("")
+            self.word_var.set(self.word_var.get() + collected_char)
             self.next_char_collect_event.set()
             time.sleep(0.1)
+
+    def udp_send(self, word_to_send):
+        bytesToSend= str.encode(word_to_send)
+        self.udp_client_socket.sendto(bytesToSend, (self.server_ip.get(), int(self.server_port.get())))
+        print("Word send")
 
     def stop_button_pressed(self):
         self.stop_button_event.set()
         self.start_button['state'] = tk.NORMAL
         self.stop_button['state'] = tk.DISABLED
+        self.server_ip_entry['state'] = tk.NORMAL
+        self.server_port_entry['state'] = tk.NORMAL
         self.root.after_cancel(self.after_id)
         self.camera.grid_forget()
         self.char.grid_forget()
